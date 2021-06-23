1359026c3c004346b65bafcdb0d39f3f95808348
unknown <nvn.bhargav@gmail.com>
2021-06-23 07:57:30 +0530

    -new component added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense'
 function App() {
 
   const expenses = [
-        {
-            id: 'e1',
-            title: 'Entertainment',
-            amount: 500,
-            date: new Date(2021, 2, 10),
-        },
-        { id: 'e2', title: 'Electricity', amount: 350, date: new Date(2021, 4, 31) },
-        {
-            id: 'e3',
-            title: 'Travel',
-            amount: 5000,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'Groceries',
-            amount: 1000,
-            date: new Date(2021, 5, 19),
-        },
-    ];
+    {
+      id: 'e1',
+      title: 'Entertainment',
+      amount: 500,
+      date: new Date(2021, 2, 10),
+    },
+    { id: 'e2', title: 'Electricity', amount: 350, date: new Date(2021, 4, 31) },
+    {
+      id: 'e3',
+      title: 'Travel',
+      amount: 5000,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'Groceries',
+      amount: 1000,
+      date: new Date(2021, 5, 19),
+    },
+  ];
   return (
     <div className="App">
-
-     
-      <Expenses items={expenses}/>
-
+      <NewExpense/>
+      <Expenses items={expenses} />
     </div>
   );
 }
